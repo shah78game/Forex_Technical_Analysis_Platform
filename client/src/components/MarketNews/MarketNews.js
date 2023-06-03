dbf2b61dfe6e303912e89804509fec5fccfee2a3
shah78game <124097061+shah78game@users.noreply.github.com>
2023-06-03 16:12:23 -0700

    Update MarketNews.js

--- client/src/components/MarketNews/MarketNews.js
+++ client/src/components/MarketNews/MarketNews.js
@@ -25,7 +25,7 @@ function MarketNews(props) {
   };
 
   useEffect(() => {
-    socket = new WebSocket('wss://d7rrbwnynf.execute-api.us-east-1.amazonaws.com/Prod');
+    socket = new WebSocket('wss://d7rrbwnynf.execute-api.us-east-1.amazonaws.com/');
 
     socket.onopen = () => {
       connectToNewsSocket();
